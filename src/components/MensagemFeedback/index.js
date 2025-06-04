ee08e5dcfc24876bfe230b4cfb0fb87b22ebf582
DaniloSSilva2 <netunoss01234@gmail.com>
2025-06-04 14:20:18 -0300

    Update index.js

--- src/components/MensagemFeedback/index.js
+++ src/components/MensagemFeedback/index.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './styles.css';
 
-function MensagemFeedback({ mensagem, tipo }) {
+function MensagemFeedback({ mensagem, tipo = "info", onFechar }) {
+  useEffect(() => {
+    if (onFechar) {
+      const timer = setTimeout(onFechar, 4000);
+      return () => clearTimeout(timer);
+    }
+  }, [onFechar]);
+
   return (
     <div className={`mensagem ${tipo}`}>
       {mensagem}
